48c0f342b817f03cf6fcbf9e8601a7f2199f7f87
michaelsustar <45545321+michaelsustar@users.noreply.github.com>
2025-03-04 23:21:55 -0500

    Create helpers.py
    creation of helpers

---
+++ helpers.py
@@ -0,0 +1,11 @@
+import statsapi
+
+def getAllPitchersFromGames(games):
+    return [[game['game_id'], game['home_probable_pitcher'], game['away_probable_pitcher']] for game in games]
+
+def findPlayerId(playerName):
+    try:
+        playerId = statsapi.lookup_player(playerName)[0]['id']
+        return playerId
+    except:
+        return 0
